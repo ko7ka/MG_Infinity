0ef2b3b2f20d5f54ec0e7cb5af42497b94c8f7e6
cl0wn65536 <hakomoriyuu@gmail.com>
2018-08-23 15:47:16 +0900

    made countdown-timer in play scene.

--- MG_Infinity(DEMO)/Assets/scripts/play/GameController.cs
+++ MG_Infinity(DEMO)/Assets/scripts/play/GameController.cs
@@ -5,30 +5,88 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject pause_scene;
-	// Use this for initialization
+	private float time = 0;
+	private Phase phase;
+
+	public GameObject pauseScene;
+	public GameObject pauseButton;
+	public GameObject touchToStart;
+	public GameObject timerController;
+	public GameObject timer;
+	public GameObject startScene;
+
+	enum Phase {
+		beforeTouchToStart,
+		afterTouchToStart,
+		playing,
+		pausing,
+		ending,
+	}
 	void Start () {
-		pause_scene.SetActive(false);
+		pauseScene.SetActive(false);
+		pauseButton.SetActive(false);
+		timerController.SetActive(false);
+		timer.SetActive(false);
+
+		phase = Phase.beforeTouchToStart;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		switch (phase)
+		{
+			case Phase.beforeTouchToStart:
+				if (Application.isEditor) {
+					if (Input.GetButtonUp("Fire1")) {
+						beforeTouchToStart();
+					}
+				} else {
+					Touch[] touches = Input.touches;
+
+					if (touches != null) {
+						beforeTouchToStart();
+					}
+				}
+				break;
+			case Phase.afterTouchToStart:
+				afterTouchToStart();
+				break;
+			case Phase.playing:
+				break;
+			case Phase.pausing:
+				break;
+			case Phase.ending:
+				break;
+		}
 	}
 
-	public void onclick_pause() {
-		pause_scene.SetActive(true);
+	void beforeTouchToStart() {
+		touchToStart.SetActive(false);
+		timerController.SetActive(true);
+		timer.SetActive(true);
+		phase = Phase.afterTouchToStart;	
+	}
+
+	void afterTouchToStart() {
+		if (timerController.activeInHierarchy == false) {
+			pauseButton.SetActive(true);
+			phase = Phase.playing;
+		}
+	}
+
+	public void onclickPause() {
+		pauseScene.SetActive(true);
 
 	}
-	public void onclick_select() {
+	public void onclickSelect() {
 		SceneManager.LoadScene("select");
 	}
 
-	public void onclick_retry() {
+	public void onclickRetry() {
 		SceneManager.LoadScene("play");
 	}
 
-	public void onclick_continue() {
+	public void onclickContinue() {
 		
 	}
 }
---
+++ MG_Infinity(DEMO)/Assets/scripts/play/TimerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour {
+	public Text timerText;
+	public float totalTime;
+	public GameObject startScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		totalTime -= Time.deltaTime;
+		
+		if (totalTime >= 1.0) {
+			timerText.text = "Ready";
+		} else if (totalTime >= 0) {
+			timerText.text = "Go";
+		} else {
+			startScene.SetActive(false);
+		}
+	}
+}
